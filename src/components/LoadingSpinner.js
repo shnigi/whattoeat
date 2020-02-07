32d529e52eee6c3d876169bf63d38a9538be43b7
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-02-07 20:25:42 +1100

    loading spinner

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -2,7 +2,10 @@ import React from "react";
 import loadingSPinnerStyles from './loadingSpinnerStyles.module.css';
 
 const LoadingSpinner = () => (
-    <div className={loadingSPinnerStyles.ldsRipple}><div></div><div></div></div>
+    <div className={loadingSPinnerStyles.loadingContainer}>
+        <h1>Loading data!</h1>
+        <div className={loadingSPinnerStyles.ldsRipple}><div></div><div></div></div>
+    </div>
 );
 
 export default LoadingSpinner;
