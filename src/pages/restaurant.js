1328c6a4a3ca9eebb90278d23474e7b08333bcc3
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-02-08 15:53:56 +1100

    disable restaurant page

--- src/pages/restaurant.js
+++ src/pages/restaurant.js
@@ -1,17 +1,17 @@
-import React from "react"
-import Card from "../components/Card";
-import appStyles from "./style.module.css";
+// import React from "react"
+// import Card from "../components/Card";
+// import appStyles from "./style.module.css";
 
-export default ({ location }) => {
-    console.log('location', location);
-    const selectedRestaurant = location.state.cards;
-    const { address1, city } = location.state.cards[0].location;
+// export default ({ location }) => {
+//     console.log('location', location);
+//     const selectedRestaurant = location.state.cards;
+//     const { address1, city } = location.state.cards[0].location;
 
-    return (
-        <div className={appStyles.appStyles}>
-            <div className={appStyles.wrapperStyles}>
-                <Card cards={selectedRestaurant}></Card>
-            </div>
-        </div>
-    )
-};
+//     return (
+//         <div className={appStyles.appStyles}>
+//             <div className={appStyles.wrapperStyles}>
+//                 <Card cards={selectedRestaurant}></Card>
+//             </div>
+//         </div>
+//     )
+// };
