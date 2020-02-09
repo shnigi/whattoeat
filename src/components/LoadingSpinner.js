3210c99e57d8653811fc59c4ca03ead112d08781
Niki Ahlskog <niki.ahlskog@gmail.com>
2020-02-09 12:02:01 +1100

    update

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,7 +3,7 @@ import loadingSPinnerStyles from './loadingSpinnerStyles.module.css';
 
 const LoadingSpinner = () => (
     <div className={loadingSPinnerStyles.loadingContainer}>
-        <h1>Loading data!</h1>
+        <h1>Loading!</h1>
         <div className={loadingSPinnerStyles.ldsRipple}><div></div><div></div></div>
     </div>
 );
